587b5ff95dd5c6157e6c1c5673b2b99bb7a714fb
kipu23 <kipu80@gmail.com>
2018-03-23 13:58:21 +0100

    Day 26 Solution - read input

--- 30 Days of Code/Day 26 Nested Logic/src/Solution.java
+++ 30 Days of Code/Day 26 Nested Logic/src/Solution.java
@@ -1,9 +1,36 @@
 import java.io.*;
 import java.util.*;
+import java.text.SimpleDateFormat;
 
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner scan = new Scanner(System.in);
+
+        int returnDay = scan.nextInt();
+        int returnMonth = scan.nextInt();
+        int returnYear = scan.nextInt();
+        Calendar returnDate = Calendar.getInstance();
+        returnDate.set(returnYear,returnMonth -1, returnDay);
+
+        scan.nextLine();
+
+        int expectedDay = scan.nextInt();
+        int expectedMonth = scan.nextInt();
+        int expectedYear = scan.nextInt();
+        Calendar expectedDate = Calendar.getInstance();
+        expectedDate.set(expectedYear,expectedMonth -1, expectedDay);
+
+
+
+
+        System.out.println(returnDate.getTime());
+        System.out.println(expectedDate.getTime());
+
+
+
+
+
     }
 }
