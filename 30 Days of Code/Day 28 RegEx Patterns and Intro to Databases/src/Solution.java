e3834f755f215e435ecbbfb7dd1964f551c8dcc2
kipu23 <kipu80@gmail.com>
2018-03-26 16:43:13 +0200

    Day28

--- 30 Days of Code/Day 28 RegEx Patterns and Intro to Databases/src/Solution.java
+++ 30 Days of Code/Day 28 RegEx Patterns and Intro to Databases/src/Solution.java
@@ -1,9 +1,53 @@
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+
+
+        Map<String, String> emails = new HashMap<>();
+
+        for (int i =0; i < n; i++){
+            scan.nextLine();
+
+            String name = scan.next();
+            String email = scan.next();
+
+            emails.put(email,name);
+
+        }
+
+        String myRegExString = ".+@gmail\\.com$";
+        Pattern p = Pattern.compile(myRegExString, Pattern.CASE_INSENSITIVE);
+
+
+        List<String> output = new ArrayList<>();
+
+        for (Map.Entry<String, String> entry : emails.entrySet()){
+
+            Matcher m = p.matcher(entry.getKey());
+
+            if( m.matches()) {
+                output.add(entry.getValue());
+            }
+
+        }
+
+        Collections.sort(output);
+
+        for (String name : output){
+            System.out.println(name);
+        }
+
+
+
     }
 }
